68cf48810ca1e3ae807630c3eb7aec4c43eaf928
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-11-11 00:35:09 +0200

    Bump version to 1.2.1

--- WorldInWindows/version.py
+++ WorldInWindows/version.py
@@ -1,6 +1,6 @@
 """Version information for World in Windows."""
 
-__version__ = "1.2.0"
-__version_info__ = (1, 2, 0)
+__version__ = "1.2.1"
+__version_info__ = (1, 2, 1)
 
 __release_date__ = "2025-11-11"
